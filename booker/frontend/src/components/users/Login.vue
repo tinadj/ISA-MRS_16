4bedd9173e730a11170ff32d15364ce0f44c110b
Kristina <djkristinaxx@gmail.com>
2019-04-20 17:45:18 +0200

    Forms for registration of new users and login #2

--- booker/frontend/src/components/users/Login.vue
+++ booker/frontend/src/components/users/Login.vue
@@ -1,10 +1,39 @@
 <template>
-    
+    <b-card-group>
+        <b-card border-variant="light" style="max-width: 20rem;" class="mb-2 mx-auto">
+            <b-form-group
+                label-for="input-username"
+            >
+                <b-form-input id="input-username" placeholder="Username"></b-form-input>
+            </b-form-group>  
+
+            <b-form-group
+                label-for="input-pass"
+            >
+                <b-form-input id="input-pass" type="password" v-model="password" placeholder="Password"></b-form-input>
+            </b-form-group>
+                    
+            <b-button variant="outline-primary" @click="signIn">Sign in</b-button><br><br>
+            <b-alert variant="danger" v-model="error" dismissible>Wrong username or password!</b-alert>
+        </b-card>
+    </b-card-group>
 </template>
 
 <script>
 export default {
-    name: 'Login'
+    name: 'Login',
+    data() {
+        return {
+            username: '',
+            password: '',
+            error: null
+        }
+    },
+    methods: {
+        signIn() {
+            this.error = null
+        }
+    }
 }
 </script>
 
