7a8db6af1b2e93e99d69308d945b450b9e3d41d2
Kristina <djkristinaxx@gmail.com>
2019-05-28 16:08:28 +0200

    Redefined navbar for rent a car admin

--- booker/frontend/src/components/admins/RentACarAdmin.vue
+++ booker/frontend/src/components/admins/RentACarAdmin.vue
@@ -5,9 +5,7 @@
         <b-nav-item :to="{ path: 'info'}"><font-awesome-icon :icon="infoIcon"/> Information</b-nav-item>
         <b-nav-item :to="{ path: 'edit' }"><font-awesome-icon :icon="editIcon"/> Edit information</b-nav-item>
         <b-nav-item :to="{ path: 'vehicles'}"><font-awesome-icon :icon="carIcon"/> Vehicles</b-nav-item>
-        <b-nav-item :to="{ path: 'add-vehicle'}"><font-awesome-icon :icon="addIcon"/> Add vehicle</b-nav-item>
         <b-nav-item :to="{ path: 'branch-offices'}"><font-awesome-icon :icon="officeIcon"/> Branch offices</b-nav-item>
-        <b-nav-item :to="{ path: 'add-branch-office'}"><font-awesome-icon :icon="addIcon"/> Add branch office</b-nav-item>
         <b-nav-item :to="{ path: 'profile-page'}"><font-awesome-icon :icon="profileIcon"/> Profile</b-nav-item>
         <b-nav-item :to="{ path: 'sign-out'}"><font-awesome-icon :icon="singOutIcon"/> Sign out</b-nav-item>
       </b-nav>
