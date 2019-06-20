e8acf7006d23f1a33c96ca752121fe78320ed383
Kristina <djkristinaxx@gmail.com>
2019-06-20 02:24:33 +0200

    Deployment try no2

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
     devServer: {
       proxy: {
         '/api': {
-          target: 'http://localhost:8081',
+          target: 'http://localhost:8080',
           ws: true,
           changeOrigin: true
         }
